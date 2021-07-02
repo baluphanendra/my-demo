3bddf14feac18173e1469a64ec13c46ef35b6179
baluphanendra <86825994+baluphanendra@users.noreply.github.com>
2021-07-02 14:20:48 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+# Add implementation
+def add(x,y):
+    return x+y
+# Subtract implementation
+def subtract(x,y):
+    return x-y         #on bug456 branch
+# Multiply implementation
+def multiply(x,y):
+    return x*y   #on master branch
+# Divide implementation
+def divide(x,y):
+    pass
+ #square implementation   
+def square(x):
+   pass 
